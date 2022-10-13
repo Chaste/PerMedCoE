bcbc16a0b8048f52bacd09c7e6f53c993d126816
Kwabena N Amponsah <kwabenantim@yahoo.com>
2022-10-13 11:20:29 +0100

    Add phase info to cell cycle writer

--- src/CellCycleWriter.cpp
+++ src/CellCycleWriter.cpp
@@ -34,6 +34,9 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "AbstractCellPopulation.hpp"
+#include "AbstractPhaseBasedCellCycleModel.hpp"
+#include "CellCyclePhases.hpp"
+
 #include "CellCycleWriter.hpp"
 
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
@@ -69,12 +72,48 @@ void CellCycleWriter<ELEMENT_DIM, SPACE_DIM>::VisitCell(CellPtr pCell, AbstractC
         *this->mpOutStream << coordinates[i] << " ";
     }
 
-    // double volume = pCellPopulation->GetVolumeOfCell(pCell);
-    double volume = pCell->GetCellData()->GetItem("volume");
-    *this->mpOutStream << volume << " ";
+    AbstractPhaseBasedCellCycleModel* p_cell_cycle_model;
+    p_cell_cycle_model = static_cast<AbstractPhaseBasedCellCycleModel*>(pCell->GetCellCycleModel());
+
+    double g1_duration = p_cell_cycle_model->GetG1Duration();
+    *this->mpOutStream << g1_duration << " ";
+
+    double s_duration = p_cell_cycle_model->GetSDuration();
+    *this->mpOutStream << s_duration << " ";
+
+    double g2_duration = p_cell_cycle_model->GetG2Duration();
+    *this->mpOutStream << g2_duration << " ";
+
+    double m_duration = p_cell_cycle_model->GetMDuration();
+    *this->mpOutStream << m_duration << " ";
+
+    CellCyclePhase current_cell_cycle_phase = p_cell_cycle_model->GetCurrentCellCyclePhase();
+    switch (current_cell_cycle_phase)
+    {
+        case G_ZERO_PHASE:
+            *this->mpOutStream << "G0 ";
+            break;
+        case G_ONE_PHASE:
+            *this->mpOutStream << "G1 ";
+            break;
+        case S_PHASE:
+            *this->mpOutStream << "S ";
+            break;
+        case G_TWO_PHASE:
+            *this->mpOutStream << "G2 ";
+            break;
+        case M_PHASE:
+            *this->mpOutStream << "M ";
+            break;
+        default:
+            NEVER_REACHED;
+    }
 
     double target_area = pCell->GetCellData()->GetItem("target area");
     *this->mpOutStream << target_area << " ";
+
+    double volume = pCell->GetCellData()->GetItem("volume");
+    *this->mpOutStream << volume << " ";
 }
 
 // Explicit instantiation
