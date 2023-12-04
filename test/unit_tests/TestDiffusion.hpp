47c2cbcb0dc4d6c9439122f4b3aaf20f0f6d7cee
Fergus Cooper <fcooper8472@gmail.com>
2023-12-04 17:06:50 +0000

    Draft of diffusion unit test

--- test/unit_tests/TestDiffusion.hpp
+++ test/unit_tests/TestDiffusion.hpp
@@ -50,6 +50,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "UblasCustomFunctions.hpp"
 
 #include "DiffusionEquationWithSinkTerm.hpp"
+#include "DiffusionEquationWithSinkTerms.hpp"
 
 /* The following header must be included in every test that uses PETSc. Note that it
  * cannot be included in the source code. */
@@ -62,7 +63,7 @@ class TestDiffusion : public CxxTest::TestSuite
 {
 
 public:
-    void Test3x3x3()
+    void xTest3x3x3()
     {
         // Target number of nodes in each dimension, so we have num_nodes x num_nodes x num_nodes in the domain
         const unsigned num_nodes = 3;
@@ -83,6 +84,9 @@ public:
 
         // Custom PDE for this unit test
         DiffusionEquationWithSinkTerm<3> pde;
+
+
+
         pde.setUptake(sink_strength);
         pde.setSinkLocation(Create_c_vector(width_xyz / 2.0, width_xyz / 2.0, width_xyz / 2.0));
         pde.setSinkRadius(sink_square_radius);
@@ -111,7 +115,7 @@ public:
         solver.SetTimeStep(dt);
 
         // Set where to output solution
-        solver.SetOutputDirectoryAndPrefix("TestDiffusionSmall3", "results");
+        solver.SetOutputDirectoryAndPrefix("TestDiffusionSmall03", "results");
         solver.SetOutputToVtk(true);
         solver.SetOutputToTxt(true);
         solver.SetPrintingTimestepMultiple(1);
@@ -126,7 +130,7 @@ public:
         TS_ASSERT_DELTA(mesh.GetNode(middle_node_idx)->rGetLocation()[1], 30.0, 1e-12);
         TS_ASSERT_DELTA(mesh.GetNode(middle_node_idx)->rGetLocation()[2], 30.0, 1e-12);
 
-        auto output_dir = FileFinder("TestDiffusionSmall3", RelativeTo::ChasteTestOutput);
+        auto output_dir = FileFinder("TestDiffusionSmall03", RelativeTo::ChasteTestOutput);
 
         auto hdf5_reader = Hdf5DataReader(output_dir, "results", "Data");
 
@@ -138,6 +142,110 @@ public:
         for (unsigned i = 0; i < time_values.size(); ++i)
         {
             std::cout << time_values.at(i) << ", " << soln_values.at(i) << '\n';
+        }
+
+        // All PETSc vectors should be destroyed when they are no longer needed.
+        PetscTools::Destroy(initial_condition);
+        PetscTools::Destroy(solution);
+    }
+
+    void Test12x12x12()
+    {
+        // Target number of points in each dimension, so we have num_pts_each_dim^3 nodes in the domain
+        const unsigned num_pts_each_dim = 12;
+
+        // Simulation parameters from PerMedCoE document
+        const double width_xyz = 330.0; // 330 x 330 x 330 micrometers,
+        const double vox_size = width_xyz / (num_pts_each_dim - 1u);
+        const double initial_concentration = 0.0; // no concentration initially
+        const double source_strength = 10.0; // constant concentration on the boundary
+        const double sink_strength = 2000.0; // 20 microMol per 0.01 minute
+        const double diffusion_coefficient = 2000.0; // 2000 micrometer^2 per minute
+
+        const double sink_square_radius = 1e-6; // sink-size, diameter in inf norm
+
+        const std::size_t num_nodes = 12ul * 12ul * 12ul;
+
+        // Create a 60 by 60 by 60 mesh in 3D. The first parameter is the cartesian space-step and the
+        // other three parameters are the width, height and depth of the mesh.
+        TetrahedralMesh<3, 3> mesh;
+        mesh.ConstructRegularSlabMesh(vox_size, width_xyz, width_xyz, width_xyz);
+
+        TS_ASSERT_EQUALS(mesh.GetNumNodes(), num_nodes);
+
+        DiffusionEquationWithSinkTerms<3> pde;
+        pde.setDiffusionCoefficient(diffusion_coefficient);
+        for (unsigned i = 0; i < 12; ++i)
+        {
+            for (unsigned j = 0; j < 12; ++j)
+            {
+                for (unsigned k = 0; k < 12; ++k)
+                {
+                    const double x_pos = static_cast<double>(i) * vox_size;
+                    const double y_pos = static_cast<double>(j) * vox_size;
+                    const double z_pos = static_cast<double>(k) * vox_size;
+                    pde.AddSink(Create_c_vector(x_pos, y_pos, z_pos), sink_strength, sink_square_radius);
+                }
+            }
+        }
+
+        TS_ASSERT_EQUALS(pde.rGetSinkLocations().size(), num_nodes);
+
+        // Create a new boundary conditions container and specify u=1.0 on the boundary.
+        BoundaryConditionsContainer<3, 3, 1> bcc;
+        bcc.DefineConstantDirichletOnMeshBoundary(&mesh, source_strength);
+
+        // Create an instance of the solver, passing in the mesh, pde and boundary conditions.
+        SimpleLinearParabolicSolver<3, 3> solver(&mesh, &pde, &bcc);
+
+        /* For parabolic problems, initial conditions are also needed. The solver will expect
+         * a PETSc vector, where the i-th entry is the initial solution at node i, to be passed
+         * in. To create this PETSc Vec, we will use a helper function in the PetscTools
+         * class to create a Vec of size num_nodes, with each entry set to 1.0. Then we
+         * set the initial condition on the solver. */
+        Vec initial_condition = PetscTools::CreateAndSetVec(mesh.GetNumNodes(), initial_concentration);
+        solver.SetInitialCondition(initial_condition);
+
+        // Next define the start time, end time, and timestep, and set them.
+        const double t_start = 0.0;
+        const double t_end = 1.0;
+        const double dt = 0.01;
+        solver.SetTimes(t_start, t_end);
+        solver.SetTimeStep(dt);
+
+        const auto num_timesteps = 1ul + static_cast<std::size_t>(std::round((t_end - t_start) / dt));
+
+        // Set where to output solution
+        solver.SetOutputDirectoryAndPrefix("TestDiffusionSmall12", "results");
+        solver.SetOutputToVtk(true);
+        solver.SetOutputToTxt(true);
+        solver.SetPrintingTimestepMultiple(1);
+
+        // Solve the model
+        Vec solution = solver.Solve();
+
+        ReplicatableVector solution_repl(solution);
+
+        auto output_dir = FileFinder("TestDiffusionSmall12", RelativeTo::ChasteTestOutput);
+        auto hdf5_reader = Hdf5DataReader(output_dir, "results", "Data");
+
+        const std::vector<double> time_values = hdf5_reader.GetUnlimitedDimensionValues();
+        const std::vector<std::vector<double> > soln_values = hdf5_reader.GetVariableOverTimeOverMultipleNodes("Variable_0", 0, num_nodes);
+        TS_ASSERT_EQUALS(time_values.size(), num_timesteps);
+        TS_ASSERT_EQUALS(soln_values.size(), num_nodes);
+        TS_ASSERT_EQUALS(soln_values.at(1).size(), num_timesteps);
+
+        for (unsigned i = 0; i < time_values.size(); ++i)
+        {
+            double average_solution = 0.0;
+            for (unsigned j = 0; j < soln_values.size(); ++j)
+            {
+                average_solution += soln_values.at(j).at(i);
+            }
+            average_solution /= soln_values.size();
+
+
+            std::cout << time_values.at(i) << ", " << average_solution << '\n';
         }
 
         // All PETSc vectors should be destroyed when they are no longer needed.
